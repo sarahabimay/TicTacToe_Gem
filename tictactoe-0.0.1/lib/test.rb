71f0e8551e432cf674456d27263ee6202d3376f6
sarahabimay <sarahabimay@gmail.com>
2016-02-02 21:46:23 +0000

    Re-build gem as it hadn't appeared to include the most recent commit

--- tictactoe-0.0.1/lib/test.rb
+++
@@ -1,6 +0,0 @@
-require 'ttt/board'
-class test
-  def foo 
-    board = Board.new(3)
-  end
-end
--- tictactoe-0.0.1/lib/tictactoe/board_options.rb
+++
@@ -1,9 +0,0 @@
-module TicTacToe
-  class BoardOptions
-    THREE_BY_THREE = 3
-
-    def self.is_valid_dimension?(dimension)
-      dimension.to_i == THREE_BY_THREE
-    end
-  end
-end
